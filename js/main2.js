10a489048e24b79aff5d2a18a30251a8fa2c16c4
Anuj Barave <anujbarave@gmail.com>
2021-10-24 16:55:40 +0530

    save

---
+++ js/main2.js
@@ -0,0 +1,66 @@
+function checkNetworkValue1() {
+    var c0 = document.getElementById('c0').value;
+    var c1 = document.getElementById('c1').value;
+    var c2 = document.getElementById('c2').value;
+    var c3 = document.getElementById('c3').value;
+    var c4 = document.getElementById('c4').value;
+    var c5 = document.getElementById('c5').value;
+    var c6 = document.getElementById('c6').value;
+    var c7 = document.getElementById('c7').value;
+    var c8 = document.getElementById('c8').value;
+    var c9 = document.getElementById('c9').value;
+    var c10 = document.getElementById('c10').value;
+    var c11 = document.getElementById('c11').value;
+    var c12 = document.getElementById('c12').value;
+    var c13 = document.getElementById('c13').value;
+    var c14 = document.getElementById('c14').value;
+    var c15 = document.getElementById('c15').value;
+    var c16 = document.getElementById('c16').value;  
+
+if(c1==c2)
+{
+    document.getElementById('btn1').style.backgroundColor = 'Green'; 
+    setTimeout(doSomething, 3000);
+
+function doSomething() {
+    document.getElementById('btn2').style.backgroundColor = 'Green';
+}
+    
+}
+if(c3==c4)
+{
+    document.getElementById('btn3').style.backgroundColor = 'Green'; 
+    document.getElementById('btn4').style.backgroundColor = 'Green';  
+}
+if(c5==c6)
+{
+    document.getElementById('btn5').style.backgroundColor = 'Green'; 
+    document.getElementById('btn6').style.backgroundColor = 'Green';  
+    
+}
+if(c7==c8)
+{
+    document.getElementById('btn7').style.backgroundColor = 'Green'; 
+    document.getElementById('btn8').style.backgroundColor = 'Green';  
+}
+if(c9==c10)
+{
+    document.getElementById('btn9').style.backgroundColor = 'Green'; 
+    document.getElementById('btn10').style.backgroundColor = 'Green';  
+}
+if(c11==c12)
+{
+    document.getElementById('btn11').style.backgroundColor = 'Green'; 
+    document.getElementById('btn12').style.backgroundColor = 'Green';  
+}
+if(c13==c14)
+{
+    document.getElementById('btn13').style.backgroundColor = 'Green'; 
+    document.getElementById('btn14').style.backgroundColor = 'Green';  
+}
+if(c15==c16)
+{
+    document.getElementById('btn15').style.backgroundColor = 'Green'; 
+    document.getElementById('btn16').style.backgroundColor = 'Green';  
+}
+}
